35cf2e9a088108a1024de1908f1089a94ac5ce12
John French <jfrench1@terpmail.umd.edu>
2019-06-03 05:53:34 -0400

    Height Of A Tree

--- src/main/java/HeightOfTree.java
+++ src/main/java/HeightOfTree.java
@@ -2,17 +2,17 @@
  * Height of a Binary Tree
  * Given a binary tree, write a method to find its height recursively.
  * An empty tree has a height of 0.
- *
+ * <p>
  * Example:
- *      1
- *     / \
- *    2   3     ==> height=3
- *   / \ / \
- *  4  5 6  7
+ * 1
+ * / \
+ * 2   3     ==> height=3
+ * / \ / \
+ * 4  5 6  7
  */
 public class HeightOfTree {
     public int findHeight(TreeNode root) {
-        if(root == null){
+        if (root == null) {
             return 0;
         }
         return 1 + Math.max(findHeight(root.left), findHeight(root.right));
