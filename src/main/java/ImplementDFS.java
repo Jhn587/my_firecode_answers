1735e64c4667a962b8c0d3e9ea9923cfed6589a1
John French <jfrench1@terpmail.umd.edu>
2019-06-07 07:02:21 -0400

    Cleaning up dfs

--- src/main/java/ImplementDFS.java
+++ src/main/java/ImplementDFS.java
@@ -5,28 +5,34 @@ import java.util.Set;
  * Graph Depth First Search
  * Implement a method to find a node in a graph using Depth First Search.
  * Example:
- *           apple
- *          /    \
- *      banana   mango
- *     /     \    /
- *  peach   strawberry
- *     \     /
- *      cherry
- *
+ * apple
+ * /    \
+ * banana   mango
+ * /     \    /
+ * peach   strawberry
+ * \     /
+ * cherry
+ * <p>
  * Find cherry ==> true
  */
 public class ImplementDFS {
     private Set<Node> visited = new HashSet<>();
-    public boolean depthFirstSearch(Node rootNode, String data){
-        if(rootNode == null || visited.contains(rootNode)){
+
+    public boolean depthFirstSearch(Node rootNode, String data) {
+        boolean result = dfsHelper(rootNode, data);
+        visited = new HashSet<>();
+        return result;
+    }
+
+    private boolean dfsHelper(Node rootNode, String data) {
+        if (rootNode == null || visited.contains(rootNode)) {
             return false;
         }
 
         visited.add(rootNode);
 
         for (Node node : rootNode.adjacentNodes) {
-            if(depthFirstSearch(node, data)){
-                this.visited = new HashSet<>();
+            if (dfsHelper(node, data)) {
                 return true;
             }
         }
